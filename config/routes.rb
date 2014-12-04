75e2b3b250d605233d77cee9d9466f9f803039c5
Yen Tran <yentran@Yens-MacBook-Pro-4.local>
2014-12-04 09:00:18 -0800

    scaffold posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
 
   resources :posts
   
+  root to: 'welcome#index'
+
   # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
