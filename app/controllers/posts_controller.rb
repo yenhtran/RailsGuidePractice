4036b2e779bd18bedfd63e684d7e6014e54876ac
Yen Tran <yentran@Yens-MacBook-Pro-4.local>
2014-12-04 09:34:05 -0800

    Save data in the Post controller 	-Strong Params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,15 @@ class PostsController < ApplicationController
 	end
 
 	def create 
-		render text: params[:post].inspect
+		# render text: params[:post].inspect -> USED TO RENDER HTML TEXT TO PAGE
+
+		@post = Post.new(params[:post_params])
+		@post.save
+		redirect_to @post
 	end
 
+	private
+		def post_params
+			params.require(:post).permit(:title, :text)
+		end
 end
